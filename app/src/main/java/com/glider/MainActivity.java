d55339c2775223b6213227db353791899edb79f1
asadinside <sheikhasadmuneer@hotmail.com>
2017-08-10 18:59:03 +0500

    images save to local storage

--- app/src/main/java/com/glider/MainActivity.java
+++ app/src/main/java/com/glider/MainActivity.java
@@ -1,36 +1,100 @@
 package com.glider;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Bitmap theBitmap = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // onCreate stuff ...
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ImageView image = (ImageView) findViewById(R.id.gif_image);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                Looper.prepare();
+                try {
+                    theBitmap = Glide.
+                            with(MainActivity.this).
+                            load("https://cdn2.iconfinder.com/data/icons/free-3d-printer-icon-set/512/Plastic_model.png").
+                            asBitmap().
+                            into(-1, -1).
+                            get();
+                } catch (final ExecutionException e) {
+
+                } catch (final InterruptedException e) {
+
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void dummy) {
+                if (null != theBitmap) {
+                    saveImageToExternal(theBitmap);
+                    Bitmap bss = getImageFromExternal();
+                    //Log.d("asad",bss.getWidth()+"");
+                }
+                ;
+            }
+        }.execute();
+    }
 
 
-        ImageView imageView = (ImageView) findViewById(R.id.gif_image);
+    public static Bitmap getImageFromExternal() {
+        final File file = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + "/AsadStuffDemo", "encore-logo.bmp");
 
-        String url ="https://cdn2.iconfinder.com/data/icons/free-3d-printer-icon-set/512/Plastic_model.png";
-        // GIF Uri
-        Uri gifUri = Uri.parse("http://p.fod4.com/p/media/22e39b6ac0/T58DAqfzR3iXMrL0EzkK_b4.gif");
-//
-//        Glide.with(this)
-//                .load(gifUri)
-//                .centerCrop()
-//                .crossFade()
-//                .into(imageView);
-//
+        if (file.exists()) {
+            Bitmap bmp = BitmapFactory.decodeFile(file.getAbsolutePath());
+            return bmp;
+        } else {
+            return null;
+        }
+    }
+    public static void saveImageToExternal(Bitmap bitmap) {
 
-        Glide.with(this)
-                .load(url)
-                .into(imageView);
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/AsadStuffDemo";
+        File dir = new File(file_path);
+        if (!dir.exists())
+            dir.mkdirs();
+        File file = new File(dir, "encore-logo" + ".bmp");
 
+        try {
+            FileOutputStream fOut = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+
+        } catch (IOException ex) {
+        }
     }
 }
